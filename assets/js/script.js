9628cec005dd4f0c1ca51e9b611857148b5c981a
williamscanlin <williamscanlin1@gmail.com>
2024-04-04 15:49:07 -0500

    update typer

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,7 +88,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["LIMS Development","Full Stack Development","Web APP Development","Back End Development","Analytical Chemistry"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
